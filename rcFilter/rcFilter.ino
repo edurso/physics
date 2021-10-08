6cee6d42959eb6f541732f8c4c9940d3da0a80e2
edurso <eric@casadurso.com>
2021-10-07 21:21:38 -0400

    update script for next stage of testing

--- rcFilter/rcFilter.ino
+++ rcFilter/rcFilter.ino
@@ -1,31 +1,49 @@
-#include "SoftPWM.h"
+const int inPin = 3;
+const int outPin = 5;
+const float speedReducer = 0.25;
+const String sep = " ";
 
-int inPin = 3;
-int outPin = 5;
-
+/**
+ * Initialization.
+ */
 void setup() {
-  SoftPWMBegin();
+
+  /* Setup Serial Port For Logging */
+  Serial.begin(9600);
+
+  /* Configure Pins */
   pinMode(inPin, INPUT);
   pinMode(outPin, OUTPUT);
-  Serial.begin(9600);
+  
 }
 
+/**
+ * Periodic Function.
+ */
 void loop() {
-  float dutyCycle = readInput();
-  int pwr = (dutyCycle * 100) / 4;
-  set(pwr);
-}
 
-void set(int pwr) {
-  //pwr 0-100%
-  SoftPWMSetPercent(outPin, pwr);
-}
+  /* Read Input */
+  unsigned long ontime = pulseIn(inPin, HIGH); // length (microseconds) of high pulse
+  unsigned long offtime = pulseIn(inPin, LOW); // length (microseconds) of low pulse
+  unsigned long period = ontime + offtime; // period (total time in microseconds for each complete pulse)
+  float freq = 1000000.0 / (float) period; // frequency (Hz)
+  float duty = ((float) ontime / period); // duty cycle ratio of ontime to total period (value between 0.0 and 1.0)
+  Serial.println(duty); // duty cycle
 
-float readInput() {
-  unsigned long highTime = pulseIn(inPin, HIGH);
-  unsigned long lowTime = pulseIn(inPin, LOW);
-  unsigned long cycleTime = highTime + lowTime;
-  float dutyCycle = (float) highTime / (float) cycleTime;
-  Serial.println(dutyCycle);
-  return dutyCycle;
+  /* Check Duty Cycle */
+  if(duty < 0.05) {
+    duty = 0.0;
+  } else if(duty > 1.0) {
+    duty = 1.0;
+  }
+  
+  /* Change Duty Cycle */
+  duty *= speedReducer;
+
+  /* Format Duty Cycle */
+  float outDuty = duty * 255;
+
+  /* Write Duty Cycle */
+  analogWrite(outPin, (int) outDuty);
+  
 }
